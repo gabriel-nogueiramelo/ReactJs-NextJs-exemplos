07d5328902a0e3b6db23f7871ca5a2f2f12e781a
gabriel-nogueiramelo <109678985+gabriel-nogueiramelo@users.noreply.github.com>
2022-10-13 22:03:18 -0300

    criado primeiro componentereact jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
+import Title from '../src/componentes/title/Title'
+
 function HomePage () {
   return (
     <div>
+      <Title />
+      <Title />
+      <Title />
       Hello Word, NextJs + ReactJs
     </div>
   )
