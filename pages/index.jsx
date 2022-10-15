6b05dfad577e27f12dd87fcea2d4b8f3fd619b23
gabriel-nogueiramelo <109678985+gabriel-nogueiramelo@users.noreply.github.com>
2022-10-15 15:13:53 -0300

    importado fonte e configurações do titulo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,21 @@
+import Head from 'next/head'
+
 import Title from '../src/componentes/title/Title'
 import Subtitle from '../src/componentes/subtitle/Subtitle'
 
 function HomePage () {
   return (
+    <>
+    <Head>
+      <title>Titulo diferente da home page</title>
+    </Head>
     <div>
       <Title>Um titulo</Title>
       <Subtitle text="Um subtitulo" />
       <Title>Outro titulo</Title>
       <Subtitle text="Outro subtitulo" />
     </div>
+    </>
   )
 }
 
