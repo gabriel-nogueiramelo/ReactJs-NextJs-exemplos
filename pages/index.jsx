cea2b9b97f4a43f5e796257ac6b2c5cec774228c
gabriel-nogueiramelo <109678985+gabriel-nogueiramelo@users.noreply.github.com>
2022-10-16 15:43:47 -0300

    criado useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import Head from 'next/head'
 
 import Title from '../src/componentes/title/Title'
 import Subtitle from '../src/componentes/subtitle/Subtitle'
+import Button from '../src/componentes/button/Button'
+
 
 function HomePage () {
+  const [click, setClick] = useState(0)
+
+const handleClick = () => {
+  setClick(click + 1)
+}
+
   return (
     <>
     <Head>
@@ -14,6 +23,12 @@ function HomePage () {
       <Subtitle text="Um subtitulo" />
       <Title>Outro titulo</Title>
       <Subtitle text="Outro subtitulo" />
+      <Button 
+      onClick={handleClick}
+      >
+        Botão teste
+        </Button>
+        <p>Cliques: {click}</p>
     </div>
     </>
   )
